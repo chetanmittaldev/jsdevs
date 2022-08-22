89575e7fd708950a98addecd9095b127b2aa7804
David Boureau <bdavidxyz@gmail.com>
2022-08-22 11:43:52 +0200

    added blog helper

---
+++ app/helpers/blog_helper.rb
@@ -0,0 +1,17 @@
+module BlogHelper
+
+  def validity_classes(is_valid, is_invalid)
+    invalid_css = "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400"
+    valid_css = "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
+    regular_css = "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+
+    res = regular_css
+    if is_invalid
+      res = invalid_css
+    elsif is_valid
+      res = valid_css
+    end
+    res
+  end
+  
+end
