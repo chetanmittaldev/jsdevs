1e076881dcd0bc12a3d7c47fc2e163dd2b9e66db
David Boureau <bdavidxyz@gmail.com>
2022-08-22 11:56:36 +0200

    fixed...

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,5 +1,4 @@
 module BlogHelper
-
   def validity_classes(is_valid, is_invalid)
     invalid_css = "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400"
     valid_css = "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
@@ -13,5 +12,4 @@ module BlogHelper
     end
     res
   end
-  
 end
