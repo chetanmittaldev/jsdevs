74bf7dd6b946b672af2fb381992c707cbe6aee6b
David Boureau <bdavidxyz@gmail.com>
2022-08-25 17:10:05 +0200

    better srb

--- test/integration/about_test.rb
+++ test/integration/about_test.rb
@@ -6,18 +6,18 @@ class AboutTest < ActionDispatch::IntegrationTest
 
     assert_select "h2", "The story"
   end
-  
+
   # TODO : fix test
   # test "renders translated versions of the markdown" do
   #   get about_path(locale: "pt-BR")
-  
+
   #   assert_select "h3", "Empoderar desenvolvedores"
   #   assert_select "li", "Empoderar desenvolvedores independentes"
   # end
-  
+
   test "defaults to English translation" do
     get about_path(locale: "zh-TW")
-    
+
     assert_select "h2", "The story"
   end
 end
