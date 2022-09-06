d8adb3b4b8ed6fee81e040046cf7403dc231fddc
David Boureau <bdavidxyz@gmail.com>
2022-09-06 10:34:28 +0200

    wip almost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < Devise::RegistrationsController
   invisible_captcha only: :create
+
+  def destroy
+    p "blablablablabla---------------------------------------"
+    p current_user
+    current_user_id = current_user.id
+    sign_out(current_user)
+    redirect_to root_path, notice: "Goodbye!"
+  end
+
 end
