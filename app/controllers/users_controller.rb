82e93f154c05105415b61edc893ec0ab73718a2b
David Boureau <bdavidxyz@gmail.com>
2022-09-06 10:50:13 +0200

    works! without tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < Devise::RegistrationsController
   invisible_captcha only: :create
 
   def destroy
-    p "blablablablabla---------------------------------------"
-    p current_user
     current_user_id = current_user.id
     sign_out(current_user)
-    redirect_to root_path, notice: "Goodbye!"
+    User.find(current_user_id).destroy
+    redirect_to root_path, status: :see_other, notice: "Goodbye!"
   end
 
 end
