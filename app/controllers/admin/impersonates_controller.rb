548f918b23fc35f9cd26e7ddadab600359d72b63
David Boureau <bdavidxyz@users.noreply.github.com>
2022-10-25 09:27:28 +0200

    Better link and impersonation

--- app/controllers/admin/impersonates_controller.rb
+++ app/controllers/admin/impersonates_controller.rb
@@ -1,7 +1,15 @@
 module Admin
   class ImpersonatesController < ApplicationController
     def create
-      user = User.find_by!(email: params[:email])
+      email = params[:email]
+      if email.include?("@")
+        user = User.find_by!(email: params[:email])
+      else
+        splitted = email.split("_")
+        biz_or_dev = splitted[0]
+        id = splitted[1]
+        user = biz_or_dev == "dev" ? Developer.find(id.to_i).user : Business.find(id.to_i).user
+      end
       impersonate_user(user)
       redirect_to root_path
     end
