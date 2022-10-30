8e424643df96ec666f92ed88bda9d05f957ff5c3
David Boureau <bdavidxyz@gmail.com>
2022-10-30 09:21:50 +0100

    added mig

--- app/controllers/admin/rates_controller.rb
+++ app/controllers/admin/rates_controller.rb
@@ -2,7 +2,9 @@ module Admin
   class RatesController < ApplicationController
     def create
       developer = Developer.find(params[:developer_id])
-      developer.update_columns(rate_params)
+      rate_params_h = rate_params
+      developer.update_columns(rate_params_h)
+      developer.update_columns(rate: rate_params_h.values.sum)
       redirect_to developers_path, notice: "Rate is done, thanks."
     end
 
--- db/migrate/20220712221760_add_rates_to_devs.rb
+++ db/migrate/20220712221760_add_rates_to_devs.rb
@@ -1,21 +1,21 @@
 class AddRatesToDevs < ActiveRecord::Migration[7.0]
   def change
-    add_column :developers, :rate, :decimal, precision: 5, scale: 2
-    add_column :developers, :rate_a, :integer
-    add_column :developers, :rate_b, :integer
-    add_column :developers, :rate_c, :integer
-    add_column :developers, :rate_d, :integer
-    add_column :developers, :rate_e, :integer
-    add_column :developers, :rate_f, :integer
-    add_column :developers, :rate_g, :integer
-    add_column :developers, :rate_h, :integer
-    add_column :developers, :rate_i, :integer
-    add_column :developers, :rate_j, :integer
-    add_column :developers, :rate_k, :integer
-    add_column :developers, :rate_l, :integer
-    add_column :developers, :rate_m, :integer
-    add_column :developers, :rate_n, :integer
-    add_column :developers, :rate_o, :integer
-    add_column :developers, :rate_p, :integer
+    add_column :developers, :rate, :decimal, precision: 5, scale: 2, default: 0
+    add_column :developers, :rate_a, :integer, default: 0
+    add_column :developers, :rate_b, :integer, default: 0
+    add_column :developers, :rate_c, :integer, default: 0
+    add_column :developers, :rate_d, :integer, default: 0
+    add_column :developers, :rate_e, :integer, default: 0
+    add_column :developers, :rate_f, :integer, default: 0
+    add_column :developers, :rate_g, :integer, default: 0
+    add_column :developers, :rate_h, :integer, default: 0
+    add_column :developers, :rate_i, :integer, default: 0
+    add_column :developers, :rate_j, :integer, default: 0
+    add_column :developers, :rate_k, :integer, default: 0
+    add_column :developers, :rate_l, :integer, default: 0
+    add_column :developers, :rate_m, :integer, default: 0
+    add_column :developers, :rate_n, :integer, default: 0
+    add_column :developers, :rate_o, :integer, default: 0
+    add_column :developers, :rate_p, :integer, default: 0
   end
 end
